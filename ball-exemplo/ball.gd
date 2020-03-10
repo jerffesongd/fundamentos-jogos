f1d31dde70e8f62bfe6e47a388eefc2cb048547b
Jerffeson Gomes Dutra <jerffeson.gomes@A305-21L.imd.local>
2020-03-10 18:18:50 -0300

    Fazendo bola se mozer com teclas do teclado

--- ball-exemplo/ball.gd
+++ ball-exemplo/ball.gd
@@ -1,5 +1,19 @@
 extends Sprite
 
+var speed_x = 100
+var speed_y = 100
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position.x += (7*delta)
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed_x * delta
+	
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed_x * delta
+	
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed_y * delta
+	
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed_y * delta
+	
